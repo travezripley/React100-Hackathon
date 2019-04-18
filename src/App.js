0533c6a0747b3ceb79f05396e900273ad46035b3
Travez Ripley <theripleys@gmail.com>
2019-04-18 08:57:48 -0700

    adds initial project files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+
+import "./App.css";
+
+import { Provider } from "./context";
+
+class App extends Component {
+    render() {
+        return (
+            <Provider>
+                <Router>
+                    <React.Fragment>
+                        <Navbar />
+                        <div className="container">
+                            <Switch>
+                                <Route exact path="/" component={Index} />
+                                <Route
+                                    exact
+                                    path="/Lyrics/track/:id"
+                                    component={Lyrics}
+                                />
+                            </Switch>
+                        </div>
+                    </React.Fragment>
+                </Router>
+            </Provider>
+        );
+    }
+}
+
+export default App;
